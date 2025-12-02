8ba0c0c3b4f55e0f5b09b6bb3cb60ee7b6008a16
melque <melque_ex@yahoo.com.br>
2025-12-02 16:02:44 -0300

    update

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ classifiers = [
  
 setup(
   name='nativeUts',
-  version='1.0.0',
+  version='1.0.1',
   description='A Python package for native utilities.',
   long_description=open('README.md').read() + '\n\n' + open('CHANGELOG.txt').read(),
   long_description_content_type='text/markdown',
